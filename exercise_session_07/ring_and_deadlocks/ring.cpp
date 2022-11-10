666ba4150502d386e9c497f2bcd071608250e7b3
Taylan Soydan <taylansoydan@gmail.com>
2022-11-10 16:19:50 +0100

    f

--- exercise_session_07/ring_and_deadlocks/ring.cpp
+++ exercise_session_07/ring_and_deadlocks/ring.cpp
@@ -5,17 +5,13 @@ int main(int argc, char** argv) {
     // Initialize the MPI environment
     int tag = 100;
     MPI_Status status;
-
-
     MPI_Init(NULL, NULL);
 
     // Get the number of processes and rank of the process
     int size, my_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	                           
-    int my_sum = my_rank;
-    
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);           
+    int total = my_rank;
     int send_rank = my_rank;  // Send    buffer
     int recv_rank = 0;        // Receive buffer
 
@@ -29,7 +25,7 @@ int main(int argc, char** argv) {
     //     update the send buffer
     //     update the local sum
 
-    for (int = 0; i < size - 1; i++) {
+    for (int i = 0; i < size - 1; i++) {
 	if (my_rank % 2) {
 		MPI_Recv(&recv_rank, 1, MPI_INTEGER, left_rank, tag, MPI_COMM_WORLD, &status);
 		MPI_Send(&send_rank, 1, MPI_INTEGER, right_rank, tag, MPI_COMM_WORLD);
@@ -39,10 +35,10 @@ int main(int argc, char** argv) {
 		MPI_Recv(&recv_rank, 1, MPI_INTEGER, left_rank, tag, MPI_COMM_WORLD, &status);
 	}
 	send_rank = recv_rank;
-	my_sum+=recv_rank;
+	total+=recv_rank;
     }
 
-    printf("I am processor %d out of %d, and the sum is %d\n", my_rank, size, my_sum);
+    printf("I am processor %d out of %d, and the sum is %d\n", my_rank, size, total);
 
     // Finalize the MPI environment.
     MPI_Finalize();
